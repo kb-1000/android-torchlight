18d2df00389fe17d58f533e522b4464b364f4edd
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-05-17 17:20:23 +0200

    Migrate to use a shared dependencies file
    Also upgrade targetSdk, Gradle plugin and downgrade Gradle to be compatible with Android Studio and the newer plugin

---
+++ dependencies.gradle.kts
@@ -0,0 +1,14 @@
+object versions {
+    const val androidGradlePlugin = "3.1.2"
+    const val gradle = "4.5.1"
+    const val androidSupport = "27.1.1"
+    const val androidSdk = 27
+}
+
+extra["versions"] = versions
+
+object gradlePlugins {
+    const val android = "com.android.tools.build:gradle:${versions.androidGradlePlugin}"
+}
+
+extra["gradlePlugins"] = gradlePlugins
