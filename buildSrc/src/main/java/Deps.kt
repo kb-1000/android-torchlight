149642b615f570e59802b7bf784c2e0d7c05d035
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-06-22 11:55:38 +0200

    Convert Gradle settings to Kotlin and introduce next dependency declaration mechanism

---
+++ buildSrc/src/main/java/Deps.kt
@@ -0,0 +1,16 @@
+object Deps {
+    object versions {
+
+        const val androidGradlePlugin = "3.1.3"
+        const val gradle = "4.5.1"
+        const val androidSupport = "27.1.1"
+        const val androidSdk = 27
+        const val timber = "4.7.0"
+    }
+
+
+    object gradlePlugins {
+        const val android = "com.android.tools.build:gradle:${versions.androidGradlePlugin}"
+    }
+
+}
