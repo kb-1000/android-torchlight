4044bfafb5f0de54fce35c0e99fac7e78ca10fba
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2018-06-23 15:14:06 +0200

    Upgrade Gradle and the Gradle wrapper to 4.8.1

--- buildSrc/src/main/java/Deps.kt
+++ buildSrc/src/main/java/Deps.kt
@@ -1,7 +1,7 @@
 object Deps {
     object versions {
         const val androidGradlePlugin = "3.1.3"
-        const val gradle = "4.5.1"
+        const val gradle = "4.8.1"
         const val androidSupport = "27.1.1"
         const val androidSdk = 27
         const val timber = "4.7.0"
